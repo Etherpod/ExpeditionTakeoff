1793bb0f28703f85c47bae41d220181f7fd92101
Etherpod <etherpod@gmail.com>
2024-05-07 18:29:50 -0700

    Init

---
+++ ExpeditionTakeoff.cs
@@ -0,0 +1,29 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace ExpeditionTakeoff;
+
+public class ExpeditionTakeoff : ModBehaviour
+{
+    public static ExpeditionTakeoff Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        ModHelper.Console.WriteLine($"My mod {nameof(ExpeditionTakeoff)} is loaded!", MessageType.Success);
+
+        LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+        {
+
+        };
+    }
+
+    public void StartTakeoffSequence()
+    {
+
+    }
+}
---
+++ PatchClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HarmonyLib;
+
+namespace ExpeditionTakeoff;
+
+[HarmonyPatch]
+public class PatchClass
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SubmitActionLoadScene), nameof(SubmitActionLoadScene.ConfirmSubmit))]
+    public static void ConfirmSubmit_Postfix(SubmitActionLoadScene __instance)
+    {
+        if (__instance._sceneToLoad == SubmitActionLoadScene.LoadableScenes.GAME
+            || __instance._sceneToLoad == SubmitActionLoadScene.LoadableScenes.EYE)
+        {
+            ExpeditionTakeoff.Instance.StartTakeoffSequence();
+        }
+    }
+}
